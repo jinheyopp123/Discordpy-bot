f889e7eed2155d2dce0a08abe59456966fe0b1f7
Sindeak or kimjinh1111 <161195769+jinheyopp123@users.noreply.github.com>
2024-07-21 12:17:58 +0900

    Update app.py

--- 001강/app.py
+++ 001강/app.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
+import sqlite3
 
+intents = discord.Intents.default()
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+
+conn = sqlite3.connect('data.db')
